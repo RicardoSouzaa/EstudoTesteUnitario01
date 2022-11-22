07aae8e979ef6b1fb8fdaa1c4a0d56d715866bd5
RIcardo Souza <ryszard696@yahoo.com.br>
2022-11-22 10:41:47 -0300

    First

---
+++ Calculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUnitario01
+{
+    public class Calculo
+    {
+        public static double Somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double Subtrair(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public static double Multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+        public static bool IsnumeroPar(int numero)
+        {
+            return numero % 2 == 0;
+        }
+    }
+}
